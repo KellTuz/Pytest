652c3d0d9b6ffe52ce5d3ca78f02d94621fcee2a
Simon Shakhanov <vonahkahs.s@gmail.com>
2024-07-06 18:41:59 +0400

    initial commit

---
+++ src/calculator.py
@@ -0,0 +1,15 @@
+from typing import Union
+
+
+class Calculator:
+    @staticmethod
+    def divide(x: Union[int, float], y: Union[int, float]) -> int | float:
+        return x / y
+
+    @staticmethod
+    def add(x: Union[int, float], y: Union[int, float]) -> int | float:
+        return x + y
+
+
+if __name__ == "__main__":
+    calculator = Calculator()
---
+++ tests/test_main.py
@@ -0,0 +1,17 @@
+from src.main import A
+
+
+def test_main():
+    assert A.x == 1
+
+
+def test_sum():
+    x = 1
+    y = 2
+    assert x + y == 3
+
+
+def test_division():
+    x = 1
+    y = 2
+    assert x / y == 0.5
